dd231a331fc330542d552eadc95a740d5e0e014a
tfoxcroftply <146833953+tfoxcroftply@users.noreply.github.com>
2023-11-13 19:01:34 +0000

    Update

--- ConsoleGame/IGTAssignment/Classes.h
+++ ConsoleGame/IGTAssignment/Classes.h
@@ -1,14 +1,17 @@
 using namespace std;
 #include <string>
+const int MapYSize = 12;
 
 class CharacterObject {
 private:
     string Sprite;
 
-    int XPos;
-    int YPos;
 public:
-    void SetSprite(string Input) {
+    int XPos = 0;
+    int YPos = 0;
+    int Color = 3;
+
+    CharacterObject(string Input) {
         Input.resize(1);
         Sprite = Input;
         return;
@@ -23,4 +26,12 @@ public:
     bool MoveY() {
 
     }
+};
+
+class MapObject {
+public:
+    int Id;
+    string MapLines[MapYSize];
+    int StartCoords[2];
+    int EndCoords[2];
 };
--- ConsoleGame/IGTAssignment/IGTAssignment.cpp
+++ ConsoleGame/IGTAssignment/IGTAssignment.cpp
@@ -11,47 +11,89 @@ using namespace std;
 #include "Classes.h"
 #include <io.h>
 #include <fcntl.h>
+#include <process.h>
 
 
 // Variables //
 int RefreshRate = 3; // Per second
-const int MapYSize = 16;
-int CurrentMap = 0;
-string MapLines[MapYSize];
-
 bool Verbose = false;
-CharacterObject Character;
+
+
+// Fixed variables //
+bool GameRunning = true;
+int PlayerScore = 0;
+bool Error = false;
+string ErrorReason = "";
+CharacterObject Character("O");
+MapObject MapData;
+
+HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+CONSOLE_CURSOR_INFO cursorInfo;
 
 
 // Functions //
-
 void vPrint(string Input) {
     if (Verbose == true) {
         cout << Input;
     }
 }
 
-void SetMap(int TargetMap) {
+void ClearConsole() { // Method prevents flickering
+    COORD coord;
+    coord.X = 0;
+    coord.Y = 0;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void Color(int Input) {
+    SetConsoleTextAttribute(Console, Input); // Shortens
+}
+
+void LogError(string Input) {
+    ErrorReason = Input;
+    Error = true;
+}
+
+void LoadMap(int TargetMap) {
     stringstream ss;
     ss << "Map" << TargetMap << ".txt";
     ifstream MapFile(ss.str());
     if (MapFile.is_open()) {
         vPrint("Opened file");
-        CurrentMap = TargetMap;
+        MapData.Id = TargetMap;
         for (int i = 0; i < MapYSize; i++) {
             string ReadLine;
             getline(MapFile, ReadLine);
-            MapLines[i] = ReadLine;
+            MapData.MapLines[i] = ReadLine;
         }
+    }
+    else {
+        LogError("The map file could not be opened. Requested map file: '" + ss.str()+ "'");
     }
 }
 
 void DisplayMap() {
+    ClearConsole();
+    cout << endl;
     for (int i = 0; i < MapYSize; i++) {
-        string str = MapLines[i];
-        wstring ws(str.begin(), str.end());
-        cout << str << endl;
+        string str = MapData.MapLines[i];
+        cout << "   ";
+        for (char& c : str)
+        {
+            Color(7);
+
+            if (c == '#') {
+                Color(8);
+            }
+
+            cout << c;
+            c = '*';
+        }
+        cout << endl;
     }
+    cout << endl;
+    Color(7);
+    cout << "      Current Level: " << MapData.Id << " | Current Score: " << PlayerScore << endl;
 }
 
 int GetKeyboardInput() {
@@ -68,10 +110,50 @@ void vPrint(string Instruction, string InputString) {
     }
 }
 
+
+void Logic() {
+    DisplayMap();
+}
+
+
+
 int main()
 {
-    Character.SetSprite("O");
-    SetMap(1);
-    DisplayMap();
+    // Init
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(Console, &cursorInfo);
+    LoadMap(1);
+    
+    // Logic loop
+    while (GameRunning == true && Error == false) {
+        Logic();
+        Sleep(1000/RefreshRate);
+    }
 
+
+    // Ending
+    system("cls");
+    Color(7);
+
+    if (Error == false) {
+        string Insertion = "";
+        if (PlayerScore > 50) {
+            Insertion = "high ";
+        }
+
+        cout << " The game has concluded!" << endl << endl;
+
+        Color(2);
+        cout << " You reached level " << MapData.Id << "!" << endl;
+        cout << " You reached a " << Insertion << "score of " << PlayerScore << "!";
+    }
+    else {
+        Color(4);
+        cout << "A fatal error has occured and the program has exited." << endl;
+        if (ErrorReason != "") {
+            cout << "The program reported an error: " << ErrorReason << endl;
+        }
+    }
+    cin.get();
 }
